f041f22acf8b16c49b42ed78703811246afa00b3
Divi123l <l.divya89@gmail.com>
2024-07-27 13:23:01 +0530

    Update FindOddNumber.java

--- myassignmentw1/FindOddNumber.java
+++ myassignmentw1/FindOddNumber.java
@@ -5,13 +5,14 @@ public class FindOddNumber {
 	public static void main(String[] args) {
 	int num = 10;
 			for (int i = 0; i <= num; i++) {
-			
-				System.out.println("Print Odd Numbers :"+i);
-				
-			
-				
+			if(i%2==1)
+		{
+		System.out.println(i+" is Odd Number");
+		}
+	}
+		
 			}
 
 	}
 
-}
+
